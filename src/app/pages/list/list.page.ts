1f0df26c2c8ef155ec1dd904860ddbccc1f44f01
marcosledesma <marcosledesmapicazo@gmail.com>
2021-01-07 19:10:02 +0100

    Añadido presentarToast en list.page.ts

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {DataService} from '../../services/data.service';
 import {Observable} from 'rxjs';
-import {IonList} from '@ionic/angular';
+import {IonList, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -15,29 +15,45 @@ export class ListPage implements OnInit {
   // Array de usuarios
   usuarios: Observable<any>;
 
-  // Añadir dataService al constructor
-  constructor(private dataService: DataService) { }
+  // Añadir dataService al constructor & añadir controlador del Toast
+  constructor(private dataService: DataService,
+              private toastCtrl: ToastController) { }
 
   // Inicio los usuarios de jsonplaceholder de data.service.ts
   ngOnInit() {
     this.usuarios = this.dataService.getUsers();
   }
 
+  // func Toast
+  async presentarToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      header: 'string',
+      color: 'success',
+      message,
+      duration: 2000,
+      position: 'middle'
+    });
+    await toast.present();
+  }
+
   // func Favoritos
-  favoritos(user: any) {
+    favoritos(user: any) {
     console.log('Favoritos', user);
+    this.presentarToast('Añadido a favoritos con éxito');
     this.lista.closeSlidingItems();
   }
 
-  // func Favoritos
-  compartir(user: any) {
+  // func Compartir
+    compartir(user: any) {
     console.log('Compartir', user);
+    this.presentarToast('Compartido con éxito');
     this.lista.closeSlidingItems();
   }
 
-  // func Favoritos
-  borrar(user: any) {
-    console.log('Borrar', user);
-    this.lista.closeSlidingItems();
-  }
+  // func Borrar
+    borrar(user: any) {
+      console.log('Borrar', user);
+      this.presentarToast('Eliminado con éxito');
+      this.lista.closeSlidingItems();
+    }
 }
