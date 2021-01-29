c7486efe688fcd74ac171a3e29e64d31ce01ce59
Marcos <marcos.ledesma.picazo@cieep.com>
2021-01-29 20:15:55 +0100

    Añadidos

--- src/app/pages/datetime/datetime.page.ts
+++ src/app/pages/datetime/datetime.page.ts
@@ -1,23 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-datetime',
-  templateUrl: './datetime.page.html',
-  styleUrls: ['./datetime.page.scss'],
+    selector: 'app-datetime',
+    templateUrl: './datetime.page.html',
+    styleUrls: ['./datetime.page.scss'],
 })
 export class DatetimePage implements OnInit {
 
-  // Vars
-  fechaNac: Date = new Date();
+    // Vars
+    fechaNac: Date = new Date();
+    customPickerOpciones: any;
+    customFecha: any;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    //
+    ngOnInit() {
+        this.customPickerOpciones = {
+            buttons: [{
+                text: 'Guardar',
+                handler: (evento) => {
+                    console.log('Clickado guardado');
+                    console.log('Evento: ', evento);
+                }
+            },
+                {
+                    text: 'Log',
+                    handler: () => {
+                        console.log('Log clickado. do not dismiss. ');
+                        return false;
+                    }
+                }
+            ]
+        };
+    }
 
-  //
-  cambioFecha(event: any) {
-    console.log('ionChange', event);
-    console.log('Fecha: ', new Date(event.detail.value));
-  }
+    //
+    cambioFecha(event: any) {
+        console.log('ionChange', event);
+        console.log('Fecha: ', new Date(event.detail.value));
+    }
 }
